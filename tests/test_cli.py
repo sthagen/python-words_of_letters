dd8991cdf42220f062309b866e8a8a09963b7749
Stefan Hagen <stefan@hagen.link>
2020-08-15 23:16:29 +0200

    Updated test for cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,12 @@ import pytest  # type: ignore
 import words_of_letters.cli as cli
 
 
-def test_main_ok_empty_array():
+def test_main_ok_empty_array(capsys):
     job = ['[]']
+    usage_feedback = """\
+    Usage: sys.args[0] <letter> <letter> ... <slots> [<slots> ...]
+    Received (['./n_worte.py']) argument vector
+    """
     assert cli.main(job) is 2
+    out, err = capsys.readouterr()
+    assert out.strip() == usage_feedback
