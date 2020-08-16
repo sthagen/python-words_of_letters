16036babc840239f4e50b444f9c43133d8f6c0dd
Stefan Hagen <stefan@hagen.link>
2020-08-16 21:04:04 +0200

    Added negative fuctional test case

--- tests/test_words_of_letters.py
+++ tests/test_words_of_letters.py
@@ -8,15 +8,25 @@ LANGUAGE_GRAMMAR = "tgerman"  # Sample for German, new grammar
 LANGUAGE_TEXT_FILE_PATH = f"tests/fixture/text/{LANGUAGE_GRAMMAR}_title.dict"
 
 
-def test_read_mixed_case_word_text_minimal_ok():
+def test_read_mixed_case_word_text_ok_minimal():
     wol.LANGUAGE_TEXT_FILE_PATH = LANGUAGE_TEXT_FILE_PATH
     word_length = 2
     assert wol.read_mixed_case_word_text(word_length) == {'AT', 'WC', 'BH', 'WM', 'WG', 'AU'}
 
 
-def test_match_gen_minimal_ok():
+def test_match_gen_ok_minimal():
     letters = ["A", "T", "W"]
     word_length = 2
     n_candidates = wol.read_mixed_case_word_text(word_length)
     matches = sorted(set(wol.match_gen(n_candidates, letters)))
     assert matches == ["AT"]
+
+
+def test_solve_nok_too_many_slots(capsys):
+    job = ["A", "B", "12"]
+    usage_feedback = (
+        'ERROR Only (2) characters given but requested (12) slots (12) ...'
+    )
+    assert wol.solve(job) == 2
+    out, err = capsys.readouterr()
+    assert out.strip() == usage_feedback
