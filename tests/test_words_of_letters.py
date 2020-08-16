381e21b984473263b00fd24aca3af6d4a21b17c8
Stefan Hagen <stefan@hagen.link>
2020-08-16 20:55:46 +0200

    Refactored fuctional test module

--- tests/test_words_of_letters.py
+++ tests/test_words_of_letters.py
@@ -4,8 +4,11 @@ import pytest  # type: ignore
 
 import words_of_letters.words_of_letters as wol
 
+LANGUAGE_GRAMMAR = "tgerman"  # Sample for German, new grammar
+LANGUAGE_TEXT_FILE_PATH = f"tests/fixture/text/{LANGUAGE_GRAMMAR}_title.dict"
+
 
 def test_read_mixed_case_word_text_minimal_ok():
-    wol.LANGUAGE_TEXT_FILE_PATH = "tests/fixture/text/tgerman_title.dict"
+    wol.LANGUAGE_TEXT_FILE_PATH = LANGUAGE_TEXT_FILE_PATH
     word_length = 2
     assert wol.read_mixed_case_word_text(word_length) == {'AT', 'WC', 'BH', 'WM', 'WG', 'AU'}
