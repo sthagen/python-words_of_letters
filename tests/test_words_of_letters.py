0d2ebb16cf26290c5a1c1655e211d97d305f4731
Stefan Hagen <stefan@hagen.link>
2020-08-23 12:26:30 +0200

    Added dump test and updated git ignore patterns to include test fixture db folder

--- tests/test_words_of_letters.py
+++ tests/test_words_of_letters.py
@@ -6,12 +6,19 @@ import words_of_letters.words_of_letters as wol
 
 LANGUAGE_GRAMMAR = "tgerman"  # Sample for German, new grammar
 LANGUAGE_TEXT_FILE_PATH = f"tests/fixture/text/{LANGUAGE_GRAMMAR}_title.dict"
+DB_BASE_PATH = f"tests/fixture/db/{LANGUAGE_GRAMMAR}_dict_"
 
 
 def test_read_mixed_case_word_text_ok_minimal():
     wol.LANGUAGE_TEXT_FILE_PATH = LANGUAGE_TEXT_FILE_PATH
     word_length = 2
     assert wol.read_mixed_case_word_text(word_length) == {'AT', 'WC', 'BH', 'WM', 'WG', 'AU'}
+
+
+def test_dump_ok_minimal():
+    wol.LANGUAGE_TEXT_FILE_PATH = LANGUAGE_TEXT_FILE_PATH
+    wol.DB_BASE_PATH = DB_BASE_PATH
+    assert wol.dump({'AT', 'WC', 'BH', 'WM', 'WG', 'AU'}) is None
 
 
 def test_match_gen_ok_minimal():
